f0d698a823f5d71bc7a0d177e8471143a753edaf
Jean Mottin <jeanmottin12@gmail.com>
2021-02-27 09:56:07 -0300

    day 1, day 2 and day 3

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,25 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+    # file = File.read("#{filename}.csv")
+    # handle_file(file)
+  end
+
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Stream.map(fn number -> String.to_integer(number) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+    # result = String.split(result, ",")
+    # result = Enum.map(result, fn number -> String.to_integer(number) end)
+    # result = Enum.sum(result)
+    # result
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
+end
